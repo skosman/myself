c6aebd72774711ddf2ffd657f309f09b0be2038a
Samantha Kosman <skosman@uvic.ca>
2022-03-16 21:52:19 -0700

    Create projects page

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,12 @@ import HelloWorld from '@/components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Hi, I'm Sam!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+         <RouterLink to="/projects">Projects</RouterLink>
       </nav>
     </div>
   </header>
@@ -94,7 +95,6 @@ nav a:first-of-type {
   header {
     display: flex;
     place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
   }
 
   header .wrapper {
